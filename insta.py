69efc527a1cd9b5a31931e25eb8f71353c5d609d
hadi-a-azeez <hadikoduvally@gmail.com>
2020-06-06 06:25:33 -0700

    log in done

--- insta.py
+++ insta.py
@@ -1,11 +1,24 @@
 from selenium import webdriver
 from time import sleep
+from
 class InstaBot:
-    def __init__(self):
+    def __init__(self,username,password):
 
         self.driver=webdriver.Chrome()
         self.driver.get("https://www.instagram.com")
         sleep(2)
-
-InstaBot()
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+            .send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+            .send_keys(password)
+        self.driver.find_element_by_xpath("//button[@type=\"submit\"]")\
+            .click()
+        sleep(4)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+        sleep(6)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+        sleep(8)
+InstaBot("hadi.a.azeez","")
         
